6da7410cd8d0b4b259c58fb92ca6e1c8f9b5f8e7
Hack Reactor Students <a.c.krause@gmail.com>
2014-05-07 15:53:59 -0700

    Helper functions to send static files, send responses, and send a 404

--- web/http-helpers.js
+++ web/http-helpers.js
@@ -1,3 +1,5 @@
+/* global exports */
+
 var path = require('path');
 var fs = require('fs');
 var archive = require('../helpers/archive-helpers');
@@ -10,9 +12,34 @@ exports.headers = headers = {
   'Content-Type': "text/html"
 };
 
+exports.send404 = function(response) {
+  exports.sendResponse(response, 'This file doesn\'t exist. Why don\'t you try zombo.com?', 404);
+};
+
+exports.sendResponse = function(response, data, status) {
+  status = status || 200;
+  response.writeHead(status, exports.headers);
+  if (typeof data !== 'string')
+  {
+    data = JSON.stringify(data);
+  }
+  response.end(data);
+};
+
 exports.serveAssets = function(res, asset) {
   // Write some code here that helps serve up your static files!
   // (Static files are things like html (yours or archived from others...), css, or anything that doesn't change often.)
+
+  fs.readFile(asset, function(err, data) {
+    if (!err) {
+      // serve file
+      exports.sendResponse(res, data.toString());
+    } else {
+      // throw 404
+      exports.send404(res);
+    }
+  });
+
 };
 
 // As you progress, keep thinking about what helper functions you can put here!
