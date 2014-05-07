c19351cb56aaf5c302c6abad2c8ade4870480ca0
Hack Reactor Students <a.c.krause@gmail.com>
2014-05-07 15:54:35 -0700

    Serve index.html when accessing root path

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,28 @@
 var path = require('path');
+var url = require('url');
 var archive = require('../helpers/archive-helpers');
+var http = require('./http-helpers.js');
 // require more modules/folders here!
 
+var getRequest = function(req, res) {
+  var urlPath = url.parse(req.url).pathname;
+
+  if (urlPath === '/') {
+    http.serveAssets(res, './public/index.html');
+  } else {
+    http.send404(res);
+  }
+};
+
+var actions = {
+  'GET': getRequest
+};
+
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+  var action = actions[req.method];
+  if (action) {
+    action(req, res);
+  } else {
+    http.send404(res);
+  }
 };
